283cb6be20609e91a8ed7f42da926e2b14d0915a
roo116 <roo116@nc.rr.com>
2022-07-21 19:35:34 -0400

    Joke and User models, package.json

---
+++ models/Joke.js
@@ -0,0 +1,43 @@
+class Joke extends Model {}
+
+// create fields/columns for User model
+Joke.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    setup: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    punchline: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "joke",
+  }
+);
+
+module.exports = Joke;
