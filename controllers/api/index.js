2166bf402b635cce31fb573dfe4eb01d1908d13c
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-07-23 13:25:57 -0400

    I can login!!

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const jokeRoutes = require("./joke-routes");
 
-router.use("/users", userRoutes);
+router.use("/user", userRoutes);
 router.use("/jokes", jokeRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,30 +40,31 @@ router.post("/", (req, res) => {
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-  })
-    .then((dbUserData) => {
-      console.log(dbUserData);
-    //   req.session.save(() => {
-    //     req.session.user_id = dbUserData.id;
-    //     req.session.username = dbUserData.username;
-    //     req.session.loggedIn = true;
+  }).then((dbUserData) => {
+    console.log(dbUserData);
+    req.session
+      .save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
         res.json(dbUserData);
       })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 });
 
 router.post("/login", (req, res) => {
   User.findOne({
     where: {
-      email: req.body.email,
+      username: req.body.username,
     },
   }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: "No user with that email address!" });
+      res.status(400).json({ message: "No user with that username!" });
       return;
     }
 
