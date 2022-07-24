7d86a5d4299e0210830f454e78a27e3107132bb1
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-24 01:26:07 -0400

    Seeds the user table with user after sign up and login is successfull

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,26 +35,41 @@ router.get("/:id", (req, res) => {
 });
 
 // add user
+// router.post("/", (req, res) => {
+//   User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password,
+//   }).then((dbUserData) => {
+//     cons
+// ole.log(dbUserData);
+//     req.session
+//       .save(() => {
+//         req.session.user_id = dbUserData.id;
+//         req.session.username = dbUserData.username;
+//         req.session.loggedIn = true;
+
+//         res.json(dbUserData);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+//   });
+// });
+
 router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-  }).then((dbUserData) => {
-    console.log(dbUserData);
-    req.session
-      .save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-
-        res.json(dbUserData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/login", (req, res) => {
--- public/js/login.js
+++ public/js/login.js
@@ -23,3 +23,33 @@ const loginFormHandler = async function (event) {
 document
   .querySelector("#login-form")
   .addEventListener("submit", loginFormHandler);
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/user", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
